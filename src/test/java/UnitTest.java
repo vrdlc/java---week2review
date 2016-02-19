9103ccf093835c67a8b002e444672d19bb4cf0f4
vrdlc <virgilioramon.delacruz@gmail.com>
2016-02-19 07:27:56 -0800

    initialized files

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class UnitTest {
+
+
+
+/*
+@Test
+public void giveChange_25cents_1quarter() {
+  App testApp = new App();
+  assertEquals("{Nickels=0, Dimes=0, Quarters=1, Pennies=0}", testApp.giveChange(25).toString());
+}
+*/
+
+}
